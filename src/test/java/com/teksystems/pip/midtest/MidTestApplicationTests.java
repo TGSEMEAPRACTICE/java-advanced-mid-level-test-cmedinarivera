9d0f19b5c4f71da4208ddcc96467706b78ccfa66
Camilo Medina <cmedinarivera@teksystems.com>
2026-01-29 17:33:57 +0100

    [feature/solution] baseline

---
+++ src/test/java/com/teksystems/pip/midtest/MidTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.teksystems.pip.midtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidTestApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
